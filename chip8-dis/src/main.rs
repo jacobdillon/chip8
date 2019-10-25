e8547d200b44687b6aec7e58eb4ce4ea55d6c280
dashaw92 <dashaw92@gmail.com>
2019-10-24 20:03:17 -0400

    Clean up main

--- chip8-dis/src/main.rs
+++ chip8-dis/src/main.rs
@@ -2,29 +2,31 @@ mod chip8;
 
 use chip8::ops::Op;
 use std::env;
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 
 fn main() {
-    let file = env::args().nth(1).and_then(|path| File::open(path).ok());
+    let path = env::args().nth(1);
 
-    if file.is_none() {
-        eprintln!("Missing expected file path as first argument or the specified file does not exist.");
+    if path.is_none() {
+        eprintln!("Missing expected file path as first argument.");
         return;
     }
-    
-    let file = file.unwrap();
-    let bytes = file.bytes()
-                    .filter_map(Result::ok)
-                    .map(|byte| u8::from_be(byte))
-                    .collect::<Vec<_>>();
-    
-    bytes.chunks(2)
-        .filter(|slice| slice.len() == 2)
-        .map(from_u8s)
-        .for_each(|byte| {
-            println!("{:04X}: {:02X?}", byte, Op::from_bin(byte));
-        });
+    let path = path.unwrap();
+
+    let bytes = fs::read(&path);
+    match bytes {
+        Ok(bytes) => {
+            let bytes: Vec<_> = bytes.iter().map(|&byte| u8::from_be(byte)).collect();
+
+            bytes.chunks(2)
+                .filter(|slice| slice.len() == 2)
+                .map(from_u8s)
+                .for_each(|byte| {
+                    println!("{:04X}: {:02X?}", byte, Op::from_bin(byte));
+                });
+        }
+        Err(_) => eprintln!("The file \"{}\" does not exist.", path),
+    }
 }
 
 fn from_u8s(slice: &[u8]) -> u16 {
